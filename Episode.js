6fc2a94c727f5376872aca09635504e3431d1d3b
William Gelhar <williamgelhar@gmail.com>
2014-06-21 14:46:31 -0400

    Adding a better way of setting _id for Episode that won't require you to always set it manually.

--- Episode.js
+++ Episode.js
@@ -1,15 +1,22 @@
 var mongoose = require('mongoose');
 
 var EpisodeSchema = mongoose.Schema( {
-    _id: { type: String, required: true },
+    _id: String,
     title: String,
     link: String,
     description: String,
     guid: String
 });
 
+EpisodeSchema.pre('save', function(next) {
+    if (!this._id) {
+        this._id = this.getID();
+    }
+    next();
+});
+
 EpisodeSchema.methods.getID = function() {
-    return this.title;
+    return this.guid || this.link;
 };
 
 var Episode = mongoose.model('Episode', EpisodeSchema);
