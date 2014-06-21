5b1715491b1dbd3721a85957c08bdb0cdd59d10a
William Gelhar <williamgelhar@gmail.com>
2014-06-21 01:21:29 -0400

    Changing Channel to use mongoose.

--- Channel.js
+++ Channel.js
@@ -1,96 +1,27 @@
-module.exports = function( db ) {
+var mongoose = require('mongoose'),
+    Episode = require('./Episode');
 
-    var Episode = require('./Episode')(db);
+var ChannelSchema = mongoose.Schema( {
+    title: String,
+    episodes: [ Episode.schema ]
+});
 
-    var Channel = function( title ) {
-        this.title = title;
+ChannelSchema.methods.getID = function() {
+    return this.title;
+};
 
-        this.episodeIDs = {};
+ChannelSchema.methods.addEpisode = function(episode) {
+    this.episodes.push(episode);
+    if (!this._addedEpisodes) {
+        this._addedEpisodes = [episode];
+    } else {
+        this._addedEpisodes.push(episode);
+    }
+};
 
-        //this list will only be populated locally, 
-        //even if values are present in the database
-        this.localEpisodes = [];
-    }; 
+var Channel = mongoose.model('Channel', ChannelSchema);
 
-    Channel.jsonChannel = function( channel ) {
-        return {
-            _id: channel.getID(),
-            title: channel.title,
-           episodes: channel.episodeIDs
-        };
-    };
-
-    Channel.prototype.getID = function() {
-        return this.title;
-    };
-
-    Channel.save = function( channel, callback ) {
-
-        //TODO: there's gotta be a better way of doing this, this is kinda silly
-        if ( Function.prototype.toString.call(channel.constructor) != Function.prototype.toString.call(Channel) ) {
-            if (callback) return callback( new TypeError('"channel" is not a Channel object') );
-        }
-
-        var collection = db.collection('channels');
-        collection.save( Channel.jsonChannel( channel ),
-            function( err, inserted ) {
-                if (err) {
-                    if (callback) return callback( err );
-                }
-            }  
-        );
-
-    };
-
-    Channel.find = function( title, callback ) {
-
-        if (!callback) {
-            callback = function(err){ if (err) throw err; };
-        }
-
-        if (!title) {
-            return callback( new TypeError('"title" is not a valid parameter') , undefined);
-        }
-
-        var channelsCollection = db.collection('channels'),
-            episodesCollection = db.collection('episodes');
-        var storedChannel = channelsCollection.findOne( { title: title},
-            function returnChannel(err, data) {
-                if (err || !data) {
-                    return callback(err, data);
-                }
-                var channel = new Channel();
-                for (var property in data) {
-                    channel[property] = data[property];
-                }
-                (function findEpisodeIDs( channelID, IDcallback ) {
-                    episodesCollection.find( 
-                        {
-                            channel: channelID
-                        },
-                        {
-                            "_id": true
-                        }
-                    ).toArray( IDcallback );
-                })( data._id, function storeIDsList(err, IDs ) {
-                    if (err) {
-                        return callback(err, channel);
-                    }
-
-                    IDs.forEach( function(element) {
-                        channel.episodeIDs[element._id] = null;
-                    } );              
-                    return callback(err, channel);
-                });
-            }
-        );
-    };
-
-    Channel.prototype.addEpisode = function ( episode ) {
-
-        //DELETE: this.episodeIDs.push( episode.getID() );
-        this.localEpisodes.push( episode );
-    };
-
-    return Channel;
+module.exports = {
+    schema: ChannelSchema,
+    model: Channel
 };
