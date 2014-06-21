f7284185676a27111110ba8ea1ba004cc231194c
William Gelhar <williamgelhar@gmail.com>
2014-06-21 01:22:24 -0400

    Adding connecting to db through mongoose in test/index.js

--- test/index.js
+++ test/index.js
@@ -2,8 +2,13 @@ var ChannelSpec = require('./ChannelSpec'),
     scrapeSpec = require('./scrapeSpec'),
     EpisodeSpec = require('./EpisodeSpec');
 
+var mongoose = require('mongoose');
+
 var dbURL = 'mongodb://localhost/TEST-podcasts';
 
+before( function() {
+    mongoose.connect(dbURL);
+} );
 
 ChannelSpec.run(dbURL);
 scrapeSpec.run(dbURL);
