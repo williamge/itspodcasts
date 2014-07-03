782227d3b8a43641d950b51abe13802c4c4e5a3f
William Gelhar <williamgelhar@gmail.com>
2014-07-02 23:25:29 -0400

    Adding a verify task for jsbeautifer to verify style, for git pre-commits, also added jshint hook for pre-commits and added githooks for making a pre-commit script.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,45 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jsbeautifier: {
-            files: ["scraper/**/*.js", "web/**/*.js"],
-            options: {
-                spaceInParen: true,
-                braceStyle: "expand"
+            "default": {
+                files: {
+                    src: ["scraper/**/*.js", "web/**/*.js"]
+                },
+                options: {
+                    spaceInParen: true,
+                    braceStyle: "expand"
+                }
+            },
+            "verify": {
+                files: {
+                    src: ["scraper/**/*.js", "web/**/*.js"]
+                },
+                options: {
+                    mode: "VERIFY_ONLY",
+                    spaceInParen: true,
+                    braceStyle: "expand"
+                }
             }
+        },
+        githooks: {
+            all: {
+                'pre-commit': 'git-pre-commit'
+            }
+        },
+        jshint: {
+            all: ["scraper/**/*.js", "web/**/*.js"]
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-jsbeautifier');
+    grunt.loadNpmTasks('grunt-githooks');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     // Default task(s).
     grunt.registerTask('default', ['jsbeautifier']);
+    grunt.registerTask('git-pre-commit', ['jsbeautifier:verify', 'jshint']);
 
 };
