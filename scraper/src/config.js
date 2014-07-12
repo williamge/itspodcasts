fb9a1f3747b1dc67b65e6e6e7501850daaec8e2c
William Gelhar <williamgelhar@gmail.com>
2014-07-12 00:50:43 -0400

    Changing the source argument reader in config.js to allow options.
    Mainly for specifying if the source should be saved to the database or not.

--- scraper/src/config.js
+++ scraper/src/config.js
@@ -1,5 +1,6 @@
 var minimistArgv = require('minimist')(process.argv.slice(2)),
-    path = require('path');
+    path = require('path'),
+    _ = require('lodash');
 
 function commandLineArguments() {
     var commandArguments = {
@@ -21,23 +22,52 @@ function commandLineArguments() {
 function podcastXMLSource() {
     var XMLSource = [];
 
+    function interpret(argument) {
+        if (!argument) {
+            return !argument;
+        }
+
+        return (new RegExp("^(y|yes|save)$", "i")).test(argument);
+    }
+
     if (minimistArgv.rss) {
+        var rssArgSplit = minimistArgv.rss.split(",");
+        var rssURL = rssArgSplit[0];
+        var saveRssSourceToDB = interpret(rssArgSplit[1]) || false;
         XMLSource.push({
             type: "rss",
-            source: minimistArgv.rss
+            source: rssURL,
+            saveToDB: saveRssSourceToDB
         });
     }
 
     if (minimistArgv.file) {
+        var fileArgSplit = minimistArgv.file.split(",");
+        var fileURL = fileArgSplit[0];
+        var saveFileSourceToDB = interpret(fileArgSplit[1]) || false;
+
         XMLSource.push({
             type: "file",
-            source: path.resolve(minimistArgv.file)
+            source: path.resolve(minimistArgv.file),
+            saveToDB: saveFileSourceToDB
         });
     }
 
     if (minimistArgv.sources) {
-        XMLSource = require(
-            path.resolve(minimistArgv.sources)
+        var sourcesFileArgSplit = minimistArgv.sources.split(",");
+        var sourcesFilePath = sourcesFileArgSplit[0];
+        var saveSourcesFileSourceToDB = interpret(sourcesFileArgSplit[1]) || false;
+
+        var sourcesFileSources = require(
+            path.resolve(sourcesFilePath)
+        );
+
+        XMLSource = _.map(sourcesFileSources,
+            function(sourceItem) {
+                return _.extend(sourceItem, {
+                    saveToDB: saveSourcesFileSourceToDB
+                });
+            }
         );
     }
 
@@ -48,5 +78,6 @@ module.exports = {
     mongoURL: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.MONGO_URL || 'mongodb://localhost/podcasts',
     cmd_args: commandLineArguments(),
     XMLSource: podcastXMLSource(),
-    softUpdate: minimistArgv['soft-update'] || false
+    softUpdate: minimistArgv['soft-update'] || false,
+    getSourcesFromDB: minimistArgv['db-sources'] || false
 };
